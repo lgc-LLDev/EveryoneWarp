98424c15d8e672615f68176cdefd83b2dfae614d
student_2333 <lgc2333@126.com>
2024-07-09 17:02:34 +0800

    change code style

--- EveryoneWarp.js
+++ EveryoneWarp.js
@@ -2,26 +2,17 @@
 // LiteLoaderScript Dev Helper
 /// <reference path="../HelperLib/src/index.d.ts"/>
 
-const pluginName = 'EveryoneWarp';
-const confDir = `plugins/${pluginName}`;
-const confPath = `${confDir}/warps.json`;
-const warpConf = new JsonConfigFile(confPath);
+const pluginName = 'EveryoneWarp'
+const confDir = `plugins/${pluginName}`
+const confPath = `${confDir}/warps.json`
+const warpConf = new JsonConfigFile(confPath)
 
-const newNavigationTask = ll.imports('NavAPI_newTask');
-const clearNavigationTask = ll.imports('NavAPI_clearTask');
-const hasNavigationTask = ll.imports('NavAPI_hasTask');
+const newNavigationTask = ll.imports('NavAPI_newTask')
+const clearNavigationTask = ll.imports('NavAPI_clearTask')
+const hasNavigationTask = ll.imports('NavAPI_hasTask')
 
-const {
-  Red,
-  DarkGreen,
-  Green,
-  Aqua,
-  White,
-  LightPurple,
-  Bold,
-  Clear,
-  MinecoinGold,
-} = Format;
+const { Red, DarkGreen, Green, Aqua, White, LightPurple, Bold, Clear, MinecoinGold } =
+  Format
 
 /**
  * @typedef {Object} FloatPosObject
@@ -49,7 +40,7 @@ const {
  * @returns {WarpData[]}
  */
 function getWarpConf() {
-  return warpConf.get('warps', []);
+  return warpConf.get('warps', [])
 }
 
 /**
@@ -57,7 +48,7 @@ function getWarpConf() {
  * @returns {boolean}
  */
 function setWarpConf(conf) {
-  return warpConf.set('warps', conf);
+  return warpConf.set('warps', conf)
 }
 
 /**
@@ -65,25 +56,25 @@ function setWarpConf(conf) {
  * @returns {string}
  */
 function formatPos(pos) {
-  const { x, y, z, dimId } = pos;
+  const { x, y, z, dimId } = pos
   const dim = (() => {
     switch (dimId) {
       case 0:
-        return '主世界';
+        return '主世界'
       case 1:
-        return '地狱';
+        return '地狱'
       case 2:
-        return '末地';
+        return '末地'
       default:
-        return '未知';
+        return '未知'
     }
-  })();
+  })()
   return (
     `${Green}${x.toFixed(2)} ` +
     `${Red}${y.toFixed(2)} ` +
     `${Aqua}${z.toFixed(2)}` +
     `${White}， ${LightPurple}${dim}`
-  );
+  )
 }
 
 /**
@@ -91,13 +82,13 @@ function formatPos(pos) {
  * @returns {string}
  */
 function formatDate(date) {
-  const yr = date.getFullYear();
-  const mon = date.getMonth() + 1;
-  const day = date.getDate();
-  const hr = date.getHours();
-  const min = date.getMinutes().toString().padStart(2, '0');
-  const sec = date.getSeconds().toString().padStart(2, '0');
-  return `${yr}-${mon}-${day} ${hr}:${min}:${sec}`;
+  const yr = date.getFullYear()
+  const mon = date.getMonth() + 1
+  const day = date.getDate()
+  const hr = date.getHours()
+  const min = date.getMinutes().toString().padStart(2, '0')
+  const sec = date.getSeconds().toString().padStart(2, '0')
+  return `${yr}-${mon}-${day} ${hr}:${min}:${sec}`
 }
 
 /**
@@ -108,14 +99,14 @@ function formatDate(date) {
  * @returns {WarpData}
  */
 function getWarpObj(pl, pos, warpName, desc = null) {
-  const { name, realName, xuid } = pl;
+  const { name, realName, xuid } = pl
   return {
     player: { name, realName, xuid },
     pos,
     name: warpName,
     date: new Date().toJSON(),
     desc,
-  };
+  }
 }
 
 /**
@@ -124,19 +115,19 @@ function getWarpObj(pl, pos, warpName, desc = null) {
  * @returns {LLSE_SimpleForm}
  */
 function addWarpButton(form, warps) {
-  let formTmp = form;
+  let formTmp = form
   warps.forEach((i) => {
     const {
       player: { name: playerName },
       pos,
       name,
-    } = i;
+    } = i
     formTmp = form.addButton(
       `${Bold}${DarkGreen}${name}${Clear}\n` +
-        `${formatPos(pos)}${Clear} - ${MinecoinGold}${playerName}`
-    );
-  });
-  return formTmp;
+        `${formatPos(pos)}${Clear} - ${MinecoinGold}${playerName}`,
+    )
+  })
+  return formTmp
 }
 
 /**
@@ -145,10 +136,10 @@ function addWarpButton(form, warps) {
 function addWarp(pl_) {
   const {
     pos: { x: x_, y: y_, z: z_, dimid: dimId_ },
-  } = pl_;
-  const strX = x_.toFixed(2).toString();
-  const strY = y_.toFixed(2).toString();
-  const strZ = z_.toFixed(2).toString();
+  } = pl_
+  const strX = x_.toFixed(2).toString()
+  const strY = y_.toFixed(2).toString()
+  const strZ = z_.toFixed(2).toString()
   const form = mc
     .newCustomForm()
     .setTitle('添加Warp')
@@ -157,14 +148,14 @@ function addWarp(pl_) {
     .addInput('Y坐标', strY, strY)
     .addInput('Z坐标', strZ, strZ)
     .addDropdown('维度', ['主世界', '地狱', '末地'], dimId_)
-    .addInput('Warp简介');
+    .addInput('Warp简介')
   pl_.sendForm(form, (pl, data) => {
     if (data) {
-      const [name, x, y, z, dimId, desc] = data;
+      const [name, x, y, z, dimId, desc] = data
 
       if (!name) {
-        pl.tell(`${Red}请输入合法内容`);
-        return;
+        pl.tell(`${Red}请输入合法内容`)
+        return
       }
 
       const pos = {
@@ -172,21 +163,21 @@ function addWarp(pl_) {
         y: y ? Number(y) : y_,
         z: z ? Number(z) : z_,
         dimId,
-      };
-      const obj = getWarpObj(pl, pos, name, desc);
+      }
+      const obj = getWarpObj(pl, pos, name, desc)
 
-      const warps = getWarpConf();
-      warps.push(obj);
-      setWarpConf(warps);
+      const warps = getWarpConf()
+      warps.push(obj)
+      setWarpConf(warps)
 
       pl.tell(
         `${Green}创建Warp ${MinecoinGold}${name} ${Green}成功！\n` +
-          `坐标：${formatPos(pos)}`
-      );
+          `坐标：${formatPos(pos)}`,
+      )
     } else {
-      pl.tell(`${Red}操作取消`);
+      pl.tell(`${Red}操作取消`)
     }
-  });
+  })
 }
 
 /**
@@ -195,19 +186,13 @@ function addWarp(pl_) {
  * @param {() => any} callback
  */
 function confirmBox(pl, tip, callback) {
-  pl.sendModalForm(
-    '确认',
-    tip,
-    `${Green}我想好了`,
-    `${Red}我手滑了`,
-    (_, res) => {
-      if (res) {
-        callback();
-      } else {
-        pl.tell(`${Red}操作取消`);
-      }
+  pl.sendModalForm('确认', tip, `${Green}我想好了`, `${Red}我手滑了`, (_, res) => {
+    if (res) {
+      callback()
+    } else {
+      pl.tell(`${Red}操作取消`)
     }
-  );
+  })
 }
 
 /**
@@ -216,7 +201,7 @@ function confirmBox(pl, tip, callback) {
  * @returns {boolean}
  */
 function compareObject(obj1, obj2) {
-  return JSON.stringify(obj1) === JSON.stringify(obj2);
+  return JSON.stringify(obj1) === JSON.stringify(obj2)
 }
 
 /**
@@ -224,27 +209,27 @@ function compareObject(obj1, obj2) {
  * @param {WarpData} warpObj
  */
 function deleteWarp(pl, warpObj) {
-  const { name, pos } = warpObj;
+  const { name, pos } = warpObj
   confirmBox(
     pl,
     `真的要删除Warp ${Bold}${MinecoinGold}${name}${Clear}` +
       `(${formatPos(pos)}${Clear}) 吗？`,
     () => {
-      const conf = getWarpConf();
-      const index = conf.findIndex((x) => compareObject(x, warpObj));
+      const conf = getWarpConf()
+      const index = conf.findIndex((x) => compareObject(x, warpObj))
       if (index === -1) {
-        pl.tell(`${Red}Warp不存在！`);
-        return;
+        pl.tell(`${Red}Warp不存在！`)
+        return
       }
-      conf.splice(index, 1);
-      setWarpConf(conf);
+      conf.splice(index, 1)
+      setWarpConf(conf)
 
       pl.tell(
         `${Green}已删除Warp ${MinecoinGold}${name}\n` +
-          `${Green}坐标： ${formatPos(pos)}`
-      );
-    }
-  );
+          `${Green}坐标： ${formatPos(pos)}`,
+      )
+    },
+  )
 }
 
 /**
@@ -254,20 +239,20 @@ function deleteWarpForm(pl_) {
   const form = mc
     .newSimpleForm()
     .setTitle('删除Warp')
-    .setContent('请选择你要删除的Warp');
+    .setContent('请选择你要删除的Warp')
   /** @type {WarpData[]} */
-  const playerWarps = [];
+  const playerWarps = []
   getWarpConf().forEach((it) => {
     if (it.player.xuid === pl_.xuid || pl_.permLevel > 0) {
-      playerWarps.push(it);
+      playerWarps.push(it)
     }
-  });
+  })
   pl_.sendForm(addWarpButton(form, playerWarps), (pl, id) => {
     if (id !== undefined && id !== null) {
-      const warp = playerWarps[id];
-      deleteWarp(pl, warp);
+      const warp = playerWarps[id]
+      deleteWarp(pl, warp)
     }
-  });
+  })
 }
 
 /**
@@ -285,9 +270,9 @@ function warpList(pl) {
       name,
       date,
       desc,
-    } = warp;
+    } = warp
 
-    const isOwner = pl_.xuid === xuid || pl.permLevel > 0;
+    const isOwner = pl_.xuid === xuid || pl.permLevel > 0
 
     let form = mc
       .newSimpleForm()
@@ -299,47 +284,47 @@ function warpList(pl) {
           `- ${DarkGreen}坐标： ${Bold}${formatPos(pos)}${Clear}\n` +
           `- ${DarkGreen}创建日期： ` +
           `${Bold}${Green}${formatDate(new Date(date))}${Clear}\n` +
-          `- ${DarkGreen}简介： ${Bold}${Green}${desc || '无'}`
+          `- ${DarkGreen}简介： ${Bold}${Green}${desc || '无'}`,
       )
-      .addButton(`${DarkGreen}导航`);
+      .addButton(`${DarkGreen}导航`)
 
-    if (isOwner) form = form.addButton(`${DarkGreen}删除`);
-    form = form.addButton(`${DarkGreen}返回Warp列表`);
+    if (isOwner) form = form.addButton(`${DarkGreen}删除`)
+    form = form.addButton(`${DarkGreen}返回Warp列表`)
 
     pl_.sendForm(form, (pl__, id) => {
       if (id !== undefined && id !== null) {
         switch (id) {
           case 0: {
-            const { xuid: xuidP } = pl__;
-            if (hasNavigationTask(xuidP)) clearNavigationTask(xuidP);
-            newNavigationTask(xuidP, warp);
-            break;
+            const { xuid: xuidP } = pl__
+            if (hasNavigationTask(xuidP)) clearNavigationTask(xuidP)
+            newNavigationTask(xuidP, warp)
+            break
           }
           case 1:
             if (isOwner) {
-              deleteWarp(pl, warp);
-              break;
+              deleteWarp(pl, warp)
+              break
             }
           // fallthrough
           case 2:
-            warpList(pl__);
-            break;
+            warpList(pl__)
+            break
           default:
         }
       }
-    });
+    })
   }
 
-  const warps = getWarpConf();
+  const warps = getWarpConf()
   const form = mc
     .newSimpleForm()
     .setTitle(pluginName)
-    .setContent(`目前已有 ${Green}${warps.length}${Clear} 个Warp`);
+    .setContent(`目前已有 ${Green}${warps.length}${Clear} 个Warp`)
   pl.sendForm(addWarpButton(form, warps), (p, i) => {
     if (i !== undefined && i !== null) {
-      warpDetail(p, warps[i]);
+      warpDetail(p, warps[i])
     }
-  });
+  })
 }
 
 /**
@@ -357,69 +342,67 @@ function warpManage(pl_) {
       if (id !== undefined && id !== null) {
         switch (id) {
           case 0:
-            addWarp(pl);
-            break;
+            addWarp(pl)
+            break
           case 1:
-            deleteWarpForm(pl);
-            break;
+            deleteWarpForm(pl)
+            break
           case 2:
-            clearNavigationTask(pl.xuid);
-            break;
+            clearNavigationTask(pl.xuid)
+            break
           default:
         }
       }
-    }
-  );
+    },
+  )
 }
 
 mc.listen('onServerStarted', () => {
-  (() => {
-    const cmd = mc.newCommand('warpmanage', '管理Warp', PermType.Any);
-    cmd.setAlias('warpm');
+  ;(() => {
+    const cmd = mc.newCommand('warpmanage', '管理Warp', PermType.Any)
+    cmd.setAlias('warpm')
 
     cmd.setCallback((_, origin, out) => {
       if (!origin.player) {
-        out.error('该命令只能由玩家执行');
-        return false;
+        out.error('该命令只能由玩家执行')
+        return false
       }
-      warpManage(origin.player);
-      return true;
-    });
+      warpManage(origin.player)
+      return true
+    })
 
-    cmd.overload();
-    cmd.setup();
-  })();
-
-  (() => {
-    const cmd = mc.newCommand('warplist', '查看Warp', PermType.Any);
-    cmd.setAlias('warp');
+    cmd.overload()
+    cmd.setup()
+  })()
+  ;(() => {
+    const cmd = mc.newCommand('warplist', '查看Warp', PermType.Any)
+    cmd.setAlias('warp')
 
     cmd.setCallback((_, origin, out) => {
       if (!origin.player) {
-        out.error('该命令只能由玩家执行');
-        return false;
+        out.error('该命令只能由玩家执行')
+        return false
       }
-      warpList(origin.player);
-      return true;
-    });
+      warpList(origin.player)
+      return true
+    })
 
-    cmd.overload();
-    cmd.setup();
-  })();
-});
-
-(() => {
-  const oldConfDir = `plugins/EveryoneWrap`;
-  const oldConfPath = `${oldConfDir}/warps.json`;
+    cmd.overload()
+    cmd.setup()
+  })()
+})
+;(() => {
+  const oldConfDir = `plugins/EveryoneWrap`
+  const oldConfPath = `${oldConfDir}/warps.json`
 
   if (file.exists(oldConfPath)) {
-    setWarpConf(new JsonConfigFile(oldConfPath).get('wraps'));
-    file.rename(oldConfPath, `${confDir}/warps_old.json`);
-    logger.info('旧的插件数据迁移完毕');
+    setWarpConf(new JsonConfigFile(oldConfPath).get('wraps'))
+    file.rename(oldConfPath, `${confDir}/warps_old.json`)
+    logger.info('旧的插件数据迁移完毕')
   }
-})();
+})()
 
 ll.registerPlugin(pluginName, '公共坐标点', [0, 2, 2], {
   Author: 'student_2333',
   License: 'Apache-2.0',
-});
+})
